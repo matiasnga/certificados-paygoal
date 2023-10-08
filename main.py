ab970a2b850e7197d368c28395024f339444df61
Matias Gallo <matias.gallo@paygoal.com.ar>
2023-10-08 09:54:36 -0300

    add db connection and json from store procedure

--- main.py
+++ main.py
@@ -1,47 +1,82 @@
-import pandas as pd
 import pdfkit
 import os
 import time
 import json
 from jinja2 import Template
+import pyodbc
+from dotenv import load_dotenv
 
-pdf_options = {
-    'page-size': 'A4',
-    'margin-top': '20mm',
-    'margin-right': '0mm',
-    'margin-bottom': '0mm',
-    'margin-left': '20mm',
-    'encoding': "UTF-8",
-    'enable-local-file-access': '',
-    'title': 'None',
-    'no-outline': None,
-    'zoom': 1.2,
-}
-def read_json_file():
-    with open('input/demo_cert_json_1.json', 'r', encoding='utf-8') as file:
-        data_dict = json.load(file)
-    return data_dict
+# Carga las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtén las variables de entorno
+db_server = os.getenv('DB_SERVER')
+db_database = os.getenv('DB_DATABASE')
+db_username = os.getenv('DB_USERNAME')
+db_password = os.getenv('DB_PASSWORD')
 
 
-def read_excel_file():
-    excel_file = "input/demo_cert.xlsx"
-    data = pd.read_excel(excel_file)
-    return data.to_dict(orient="records")
+def establecer_conexion():
+    try:
+        # Configura la cadena de conexión
+        server = db_server
+        database = db_database
+        username = db_username
+        password = db_password
+
+        # Crea la cadena de conexión
+        conn = pyodbc.connect(
+            'DRIVER={SQL Server};'
+            f'SERVER={server};'
+            f'DATABASE={database};'
+            f'UID={username};'
+            f'PWD={password};'
+        )
+
+        return conn
+    except pyodbc.Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
+
+
+# Intenta establecer la conexión
+conexion = establecer_conexion()
+data_dict = dict
+# Verifica si la conexión se estableció correctamente
+if conexion:
+    print("La conexión se estableció correctamente.")
+    cursor = conexion.cursor()
+    cursor.execute('EXEC GetJsonData')
+    resultado = cursor.fetchone()[0]
+
+    # Imprime el resultado
+    print(resultado)
+    data_dict = json.loads(resultado)
+
+    # Cierra el cursor y la conexión
+    cursor.close()
+    # Aquí puedes continuar con la ejecución de tu código, incluida la ejecución del procedimiento almacenado.
+else:
+    print("La conexión no se pudo establecer. Revise la configuración de conexión.")
+
+
+# def read_json_file():
+#     with open('input/demo_cert_json_1.json', 'r', encoding='utf-8') as file:
+#         data_dict = json.load(file)
+#     return data_dict
 
 
 tiempo_inicio_proceso = time.time()
-# lista_certificados = read_excel_file()
-lista_certificados = read_json_file()
-
-
+lista_certificados = data_dict
 
 # Recorre la lista de certificados
+
 for i, certificado in enumerate(lista_certificados):
     tiempo_inicio_iteracion = time.time()
-    cuit = ''.join([caracter for caracter in str(certificado["CUITContribuyente"]) if caracter != "-"])
+    cuit = certificado['CUITContribuyente'].replace("-", "")
+    withholdingGroupingId = certificado['CUITContribuyente']
     output_path = "output/" + cuit + "/"
-    output_file = str(certificado["Impuesto"]) + " - " + str(
-        certificado["NroCertificado"]) + ".pdf"
+    output_file = str(certificado["Impuesto"]) + " - " + certificado["NroCertificado"] + ".pdf"
     titulo_pdf = output_file
 
     # Crea el directorio de salida si no existe
@@ -53,6 +88,7 @@ for i, certificado in enumerate(lista_certificados):
         template_html = template_file.read()
         template = Template(template_html)
 
+    certificado['firma_src'] = './template/Firma_' + certificado['CUITAgente'].replace("-", "") + '.png'
     # Renderizar la plantilla con los datos
     # Inserta el certificado en el HTML
     html_template = template.render(**certificado)
@@ -61,11 +97,23 @@ for i, certificado in enumerate(lista_certificados):
     with open('temp.html', 'w', encoding='utf-8') as f:
         f.write(html_template)
 
+    pdf_options = {
+        'page-size': 'A4',
+        'margin-top': '20mm',
+        'margin-right': '0mm',
+        'margin-bottom': '0mm',
+        'margin-left': '20mm',
+        'encoding': "UTF-8",
+        'enable-local-file-access': '',
+        'title': output_file,
+        'no-outline': None,
+        'zoom': 1.2,
+    }
     # Genera el archivo PDF
     pdfkit.from_file('temp.html', output_path + output_file, options=pdf_options)
 
     # Elimina el archivo temporal HTML
-    # os.remove("temp.html")
+    os.remove("temp.html")
 
     tiempo_fin_iteracion = time.time()
     tiempo_iteracion = tiempo_fin_iteracion - tiempo_inicio_iteracion
