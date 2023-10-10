eb90c71077f9dce9ae46d12d8f56b7711e17998a
Matias Gallo <matias.gallo@paygoal.com.ar>
2023-10-10 15:38:15 -0300

    refactor

--- db.py
+++ db.py
@@ -1,29 +1,20 @@
 import json
+from datetime import datetime
+
 import pyodbc
-from dotenv import load_dotenv
-import os
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("certificados-paygoal-db")
 
-# Carga las variables de entorno desde el archivo .env
-load_dotenv()
 
-# Obtén las variables de entorno
-db_server = os.getenv('DB_SERVER')
-db_database = os.getenv('DB_DATABASE')
-db_username = os.getenv('DB_USERNAME')
-db_password = os.getenv('DB_PASSWORD')
-
-
-def establecer_conexion():
+def establecer_conexion_replica():
     try:
         # Configura la cadena de conexión
-        server = db_server
-        database = db_database
-        username = db_username
-        password = db_password
+        server = 'localhost'
+        database = 'PaygoalWHT'
+        username = 'admin'
+        password = 'w)7m!=_8V}'
 
         # Crea la cadena de conexión
         conn = pyodbc.connect(
@@ -40,19 +31,64 @@ def establecer_conexion():
         return None
 
 
-def execute_sp(cuit_agente, periodo, id_impuesto, cuit_contribuyente):
-    conexion = establecer_conexion()
+def establecer_conexion_dev():
+    try:
+        # Configura la cadena de conexión
+        server = 'localhost'
+        database = 'PaygoalWHT_dev'
+        username = 'admin'
+        password = '!=_8V}w)7m'
+
+        # Crea la cadena de conexión
+        conn = pyodbc.connect(
+            'DRIVER={SQL Server};'
+            f'SERVER={server};'
+            f'DATABASE={database};'
+            f'UID={username};'
+            f'PWD={password};'
+        )
+
+        return conn
+    except pyodbc.Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
+
+
+def execute_sp(db, cuit_agente, periodo, id_impuesto, cuit_contribuyente):
+    global conexion
+
+    if db == 'dev':
+        conexion = establecer_conexion_dev()
+    elif db == 'replica':
+        conexion = establecer_conexion_replica()
 
     if conexion:
-        logger.info("La conexión a la base de datos se estableció correctamente.")
+        logger.info("La conexión a la base de datos se estableció correctamente." + db)
         cursor = conexion.cursor()
         cursor.execute('EXEC WithholdingCertificatesByShop ?, ?, ?, ?', cuit_agente, periodo, id_impuesto,
                        cuit_contribuyente)
-        resultado = cursor.fetchone()[0]
-        lista_certificados = json.loads(resultado)
+        resultado = cursor.fetchall()
+
+        # Inicializa una cadena para concatenar los resultados
+        concatenated_result = ""
+        print(len(resultado))
+        i = 0
+        lista_certificados = ''
+        # Itera a través de las filas y concatena el contenido
+        for i in range(len(resultado)):
+            row = str(resultado[i])[2:-3]
+            # Concatena el contenido de cada fila
+            lista_certificados += row
+            i += 1
+        with open('input_data/sp_data_' + cuit_agente + '_' + periodo + '_' + db + '.txt', 'w', encoding='utf-8') as archivo:
+            archivo.write(lista_certificados)
+
+        print(f"El string se ha guardado en el archivo sp_data.txt.")
+        lista_certificados_str = json.loads(str(lista_certificados))
+
         # Cierra el cursor y la conexión
         cursor.close()
         conexion.close()
-        return lista_certificados
+        return lista_certificados_str
     else:
         logger.info("La conexión a la base de datos no se pudo establecer.")
--- main.py
+++ main.py
@@ -1,42 +1,49 @@
-import utils
-import os
 import db
+import utils
 import logging
-import shutil
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("certificados-paygoal")
-while True:
-    entrada = input("Presione 1 para generar 'MODO' o 2 para generar 'PAYWAY': ")
-    if entrada == "1":
-        cuit_agente = "30716829436"
-        break  # Sal del bucle si la entrada es válida
-    elif entrada == "2":
-        cuit_agente = "30598910045"
-        break  # Sal del bucle si la entrada es válida
-    else:
-        print("Entrada no válida. Intente nuevamente.")
+logger = logging.getLogger('certificados-paygoal')
 
-periodo = input("Ingrese periodo yyyyMM: ")
-
-id_impuesto = input("Ingrese ID de impuesto: ")
-cuit_contribuyente = input("Ingrese CUIT del contribuyente: ")
+ambiente = 'replica'
+cuit_agente = '30716829436'
+periodo = '202309'
+id_impuesto = None
+cuit_contribuyente = None
+#
+output_path = 'certificados/' + cuit_agente + '/' + periodo
+output_zip = 'certificados/' + cuit_agente
 
 
-lista_certificados = db.execute_sp(cuit_agente, periodo, id_impuesto, cuit_contribuyente)
+# db.execute_sp(ambiente, cuit_agente, periodo, id_impuesto, cuit_contribuyente)
+lista_certificados = utils.open_json('input_data/sp_data_30716829436_202309_replica.txt')
+logger.info(lista_certificados)
 
-output_path = "certificados/" + cuit_agente + '/' + periodo
-
-if os.path.exists(output_path):
-    shutil.rmtree(output_path)
-
+archivo_zip = output_path + '/certificados.zip'
+#
 utils.generate_pdf(lista_certificados, output_path)
 
-archivo_zip = output_path + '/certificados_' + cuit_agente + '_' + periodo + '.zip'
+# utils.generar_zip(output_zip, archivo_zip)
 
-utils.generar_zip(output_path, archivo_zip)
-# shutil.make_archive(archivo_zip, 'zip', output_path)
+# utils.analizar_zip(archivo_zip)
+# carpeta1 = "certificados_QA/node/"
+# carpeta2 = "certificados_QA/python/"
+#
+# # Obtén una lista de archivos en la carpeta 1
+# archivos_carpeta1 = os.listdir(carpeta1)
+#
+# # Itera sobre la lista de archivos y compara cada par de archivos
+# for nombre_archivo in archivos_carpeta1:
+#     archivo1 = os.path.join(carpeta1, nombre_archivo)
+#     archivo2 = os.path.join(carpeta2, nombre_archivo)
+#
+#     if os.path.isfile(archivo2):
+#         son_iguales = utils.comparar_pdf(archivo1, archivo2)
+#
+#         # Haz algo con el resultado 'son_iguales', por ejemplo, imprimirlo
+#         logger.info(f'Son iguales? {son_iguales}')
+#     else:
+#         logger.info(f'El archivo {nombre_archivo} no existe en {carpeta2}')
 
-utils.analizar_zip(archivo_zip)
 
 logger.info('*** PROCESO FINALIZADO ***')
